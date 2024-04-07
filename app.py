46628a7ee82680f51a77ca07ec643fdbfabc46b3
yoshiaki <yoshiaki110@gmail.com>
2024-04-07 14:37:53 +0900

    opencv import

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import cv2
+
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    return 'Hello from Koyeb!!!'
  
  
 if __name__ == "__main__":
